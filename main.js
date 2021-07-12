93a185424e44ed3e0f9c40fe7d4ac73619e01d1c
jiseonBaek <js.coding.place@gmail.com>
2021-07-12 22:01:01 +0900

    fix: 게임시작 시 당근 배치 기능

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 'use strict'
+
+const carrot = document.querySelector('.carrot__group');
+const carrotIdx = document.querySelectorAll('.carrot');
+let carrots = [];
+
+
+const removeCarrot = (idx) => {
+	
+	carrotIdx.addEventListener('click', () => {
+		
+	})
+}
+
+
+const showCarrorts = () => {
+	carrot.classList.remove('hide');
+}
+
+
+const startGame = () => {
+	const startBtn = document.querySelector('.start__btn');
+
+	startBtn.addEventListener('click', () => {
+		showCarrorts();
+	})
+}
+
+
+function init() {
+	carrot.classList.add('hide');	
+	startGame();
+	
+	
+}
+
+init();
